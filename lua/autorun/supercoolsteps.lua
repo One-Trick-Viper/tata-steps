5e834d3d1613dc98da8658003d0a049a7a47601c
Jake <rapidviperman@outlook.com>
2025-08-18 16:44:59 +0200

    Update supercoolsteps.lua

--- lua/autorun/supercoolsteps.lua
+++ lua/autorun/supercoolsteps.lua
@@ -2,7 +2,7 @@ local weaponcategories = {
     ["Assault Rifles"] = "rattle.rifle",
     ["Pistols"] = "rattle.pistol",
     ["Lightmachine Guns"] = "rattle.lmg",
-    ["Submachine Guns"] = "rattle.smg",
+    ["Submachine Guns"] = "rattle.smg"
 }
 
 local surfacefootsteps = {
@@ -12,7 +12,7 @@ local surfacefootsteps = {
     [MAT_METAL] = "metal.walk",
     [MAT_SNOW] = "snow.walk",
     [MAT_WOOD] = "wood.walk",
-    [MAT_TILE] = "tile.walk",
+    [MAT_TILE] = "tile.walk"
 }
 
 local surfacefootstepssprinting = {
@@ -22,78 +22,81 @@ local surfacefootstepssprinting = {
     [MAT_METAL] = "metal.sprint",
     [MAT_SNOW] = "snow.sprint",
     [MAT_WOOD] = "wood.sprint",
-    [MAT_TILE] = "tile.sprint",
+    [MAT_TILE] = "tile.sprint"
 }
 
-hook.Add( "PlayerFootstep", "CustomFootstep", function( ply, pos, foot, sound, volume, rf )
+hook.Add("PlayerFootstep", "CustomFootstep", function(ply, pos, foot, sound, volume, rf)
     local weapon = ply:GetActiveWeapon()
     local armorvalue = ply:Armor()
     -- print(weapon.SubCategory)
     -- print(weaponcategories[weapon.SubCategory])
     local sprinting = ply:KeyDown(IN_SPEED) and surfacefootstepssprinting or surfacefootsteps
-    	local tr = util.TraceLine( {
-		start = ply:GetPos(),
-		endpos = ply:GetPos() + Vector(0,0,-10),
+    local tr = util.TraceLine({
+        start = ply:GetPos(),
+        endpos = ply:GetPos() + Vector(0, 0, -10),
         filter = ply
-	} )
+    })
     -- print(tr.MatType)
     local materialtype = tr.MatType or MAT_CONCRETE
     -- PrintTable(tr)
 
-    if (sprinting[materialtype]) then --lets play the sounds lol
+    if (sprinting[materialtype]) then -- lets play the sounds lol
         ply:EmitSound("scuffle.tarmac")
         ply:EmitSound(sprinting[materialtype])
         ply:EmitSound(ply:KeyDown(IN_SPEED) and "clothing.sprint" or "clothing.walk")
 
-    if IndoorCheck(ply) then
-        if (materialtype == MAT_CONCRETE or materialtype == MAT_TILE or materialtype == MAT_METAL or materialtype == MAT_WOOD) and IndoorCheck(ply)  then
-        ply:EmitSound(ply:KeyDown(IN_SPEED) and "indoor.sprint" or "indoor.walk")
+        if IndoorCheck(ply) then
+            if (materialtype == MAT_CONCRETE or materialtype == MAT_TILE or materialtype == MAT_METAL or materialtype ==
+                MAT_WOOD) and IndoorCheck(ply) then
+                ply:EmitSound(ply:KeyDown(IN_SPEED) and "indoor.sprint" or "indoor.walk")
+            end
         end
     end
-end
 
-    if armorvalue > 120 then 
+    if armorvalue > 120 then
         ply:EmitSound(ply:KeyDown(IN_SPEED) and "superheavy.sprint" or "superheavy.walk")
     elseif armorvalue > 60 then
         ply:EmitSound(ply:KeyDown(IN_SPEED) and "heavy.sprint" or "heavy.walk")
-    elseif  armorvalue > 30 then
+    elseif armorvalue > 30 then
         ply:EmitSound(ply:KeyDown(IN_SPEED) and "medium.sprint" or "medium.walk")
     elseif armorvalue > 15 then
         ply:EmitSound(ply:KeyDown(IN_SPEED) and "light.sprint" or "light.walk")
     end
 
     if (weaponcategories[weapon.SubCategory]) and ply:KeyDown(IN_SPEED) then
-        if (ply.RattleTime or 0) < CurTime() then 
-                ply:EmitSound(weaponcategories[weapon.SubCategory])-- Play weapon rattle sound
-                ply.RattleTime = CurTime() + 0.5
+        if (ply.RattleTime or 0) < CurTime() then
+            ply:EmitSound(weaponcategories[weapon.SubCategory]) -- Play weapon rattle sound
+            ply.RattleTime = CurTime() + 0.5
         end
     end
 
     return true
-end )
+end)
 
-hook.Add( "OnPlayerHitGround", "LandSound", function(ply) 
+hook.Add("OnPlayerHitGround", "LandSound", function(ply)
     ply:EmitSound("land")
 end)
 
 function IndoorCheck(ply)
-    local tr = util.TraceLine( {
-		start = ply:GetPos(),
-		endpos = ply:GetPos() + Vector(0,0,500),
+    local tr = util.TraceLine({
+        start = ply:GetPos(),
+        endpos = ply:GetPos() + Vector(0, 0, 500),
         filter = ply
-	} )
+    })
     -- print("its a roof lol")
-    return tr.Hit and not tr.HitSky 
+    return tr.Hit and not tr.HitSky
 end
 
 hook.Add("PlayerTick", "PlayerBreathing", function(ply)
-   if CLIENT then return end 
-   if ply:KeyDown(IN_SPEED) and ply:IsOnGround() and ply:GetVelocity():Length() > 100 then
+    if CLIENT then
+        return
+    end
+    if ply:KeyDown(IN_SPEED) and ply:IsOnGround() and ply:GetVelocity():Length() > 100 then
         if ply.BreathingStartTime == nil then
             ply.BreathingStartTime = CurTime()
         end
         print(ply.BreathingStartTime)
-        if CurTime() > ply.BreathingStartTime + 10 then 
+        if CurTime() > ply.BreathingStartTime + 10 then
             if ply.BreathingPlayed == nil then
                 ply:EmitSound("breath.loop")
                 ply.BreathingPlayed = true
@@ -105,7 +108,7 @@ hook.Add("PlayerTick", "PlayerBreathing", function(ply)
             ply:EmitSound("breath.end")
         end
         ply.BreathingPlayed = nil
-    end 
+    end
 
     if ply:KeyDown(IN_DUCK) and ply:IsOnGround() then
         if ply.PlayerCrouched == nil then
